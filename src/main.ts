c68801616905e8c2130d2a7e75750d0310559378
gitdevjin <dev.hjshin@gmail.com>
2025-12-14 16:29:00 -0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CLIENT_URL } from './lib/const';
 
 async function bootstrap() {
+  const allowedOrigins = [CLIENT_URL, 'https://another-client.com', 'http://localhost:5173'];
+
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,10 @@
 import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'Please enter a valid Email address' })
   email: string;
 
   @IsString()
-  @Length(8, 20)
+  @Length(8, 20, { message: 'Password must be between 8 and 20 characters' })
   password: string;
 }
